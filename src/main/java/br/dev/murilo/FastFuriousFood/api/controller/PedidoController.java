af84dc0a858f51d424df4221056e9964a61bd245
murilo <mumurilosiq@gmail.com>
2026-04-08 17:13:06 -0300

    Inicio da api fast furious food

---
+++ src/main/java/br/dev/murilo/FastFuriousFood/api/controller/PedidoController.java
@@ -0,0 +1,10 @@
+package br.dev.murilo.FastFuriousFood.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PedidoController 
+{
+  
+    
+}
---
+++ src/test/java/br/dev/murilo/FastFuriousFood/FastFuriousFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.murilo.FastFuriousFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFuriousFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
